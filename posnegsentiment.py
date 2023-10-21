744c8a1ed656b939efd7c30cdcd5ffddd17e837d
Surya Shetty <54557065+Suraskey@users.noreply.github.com>
2023-10-21 18:04:28 -0500

    Add files via upload
    The provided Python code performs sentiment analysis on a collection of sports articles using the VADER sentiment analysis model from the Natural Language Toolkit (NLTK). It begins by initializing the sentiment analyzer and then proceeds to analyze the sentiment of each sports article in the list.
    
    For each article, the code calculates sentiment scores, including positive, negative, neutral, and compound scores, which provide an overall measure of sentiment. Finally, it determines the overall sentiment of the collection of articles based on the average compound score.
    
    This code can be customized to analyze the sentiment of sports articles from various sources and may serve as a starting point for sentiment analysis in sports-related projects.

---
+++ posnegsentiment.py
@@ -0,0 +1,30 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+file = open('/Users/suryashetty/PycharmProjects/NetProphets/article.txt','r')
+content = file.read()
+
+sports_articles = [
+    content
+]
+
+analyzer = SentimentIntensityAnalyzer()
+
+for i, article in enumerate(sports_articles, 1):
+    sentiment = analyzer.polarity_scores(article)
+    print(f"Article {i} Sentiment Analysis:")
+    print("Article Text: ", article)
+    print("Sentiment Scores (Positive/Negative/Neutral/Compound):", sentiment)
+    print()
+
+compound_scores = [analyzer.polarity_scores(article)["compound"] for article in sports_articles]
+average_compound_score = sum(compound_scores) / len(compound_scores)
+
+if average_compound_score >= 0.05:
+    overall_sentiment = "positive"
+elif average_compound_score <= -0.05:
+    overall_sentiment = "negative"
+else:
+    overall_sentiment = "neutral"
+
+print(f"Overall Sentiment of the Sports Articles: {overall_sentiment}")
