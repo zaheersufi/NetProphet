1a1a45317aee74352f348b0ef01f78ae359b702a
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2023-11-19 02:19:38 -0600

    br

--- scrape.py
+++ scrape.py
@@ -24,17 +24,24 @@ for i in range(1997, 2025):
     url = 'https://www.basketball-reference.com/leagues/NBA_' + str(i) + '_advanced.html'
     team_url = "https://www.basketball-reference.com/leagues/NBA_" + str(i) + "_ratings.html"
 
+
+
     req = requests.get(url, headers)
     soup  = BeautifulSoup(req.content, 'html.parser')
     req_team = requests.get(team_url, headers)
     soup_team = BeautifulSoup(req_team.content, "html.parser")
 
+    per_poss_url = "https://www.basketball-reference.com/leagues/NBA_" + str(i) + "_per_poss.html"
+    per_poss = requests.get(per_poss_url, headers)
+    soup_per_pous = BeautifulSoup(req_team.content, 'html.parser')
 
     team = -1
     team_set = {"wins", "def_rtg"}
     teams = {-1: ['0', '1000']}
     table = soup_team.find("tbody")
     rows = table.find_all("tr")
+    table_per = soup_per_pous.find("tbody")
+    rows_per = table_per.find_all("tr")
     for row in rows:
         data = []
         name = row.find(class_="left")
@@ -50,11 +57,11 @@ for i in range(1997, 2025):
     table = soup.find("tbody") 
     rows = table.find_all("tr")
     dict2022 = {}
-    for row in rows:
+    for row in range(len(rows)):
         info = []
-        if "thead" not in row.get("class"):
-        #   players.append(row.find_all("a")[0].text)
-            elements = row.find_all("a")
+        if "thead" not in rows[row].get("class"):
+        # players.append(row.find_all("a")[0].text)
+            elements = rows[row].find_all("a")
             name = elements[0].text
             if len(elements) == 1:
                 #dict2022[name] = "NONE"
@@ -63,12 +70,15 @@ for i in range(1997, 2025):
                 team = elements[1].text
                 #dict2022[name] = team
                 info.append(team)
-            for cell in row:            
+            for cell in rows[row]:            
                 data_type = cell.get("data-stat")
                 if data_type in stats_set:
                     info.append(cell.text)
-            print(team)
             info += teams[team]
+            for cell in rows_per[row]:
+                data_type = cell.get("data-stat")
+                if data_type == "def_rtg":
+                    info.append(cell.text)
             dict2022[name] = info
     total[str(i)] =  dict2022
     time.sleep(5)
--- test.py
+++ test.py
@@ -12,18 +12,20 @@ headers = {
 }
 
 
-url = 'https://www.basketball-reference.com/leagues/NBA_' + str(1997) + '_advanced.html'
+url = 'https://www.basketball-reference.com/leagues/NBA_' + str(2023) + '_advanced.html'
 
 req = requests.get(url, headers)
 soup  = BeautifulSoup(req.content, 'html.parser')
 
 
-team = "https://www.basketball-reference.com/leagues/NBA_" + str(1997) + "_ratings.html"
+team = "https://www.basketball-reference.com/leagues/NBA_" + str(2023) + "_ratings.html"
 req_team = requests.get(team, headers)
 soup_team  = BeautifulSoup(req_team.content, 'html.parser')
 
 
-
+per_poss_url = "https://www.basketball-reference.com/leagues/NBA_" + str(2023) + "_per_poss.html"
+per_poss = requests.get(per_poss_url, headers)
+soup_per_pous = BeautifulSoup(req_team.content, 'html.parser')
 
 
 team_set = {"wins", "def_rtg"}
@@ -44,16 +46,19 @@ for row in rows:
 print(teams)
 
 
+
 stats_set = {"g", "per", "ws", "ws_per_48", "bpm", "vorp", "dws", "dbpm", "blk_pct", "stl_pct"}
 
 table = soup.find("tbody")
 rows = table.find_all("tr")
+table_per = soup_per_pous.find("tbody")
+rows_per = table_per.find_all("tr")
 dict2022 = {}
-for row in rows:
+for row in range(len(rows)):
     info = []
-    if "thead" not in row.get("class"):
+    if "thead" not in rows[row].get("class"):
     # players.append(row.find_all("a")[0].text)
-        elements = row.find_all("a")
+        elements = rows[row].find_all("a")
         name = elements[0].text
         if len(elements) == 1:
             #dict2022[name] = "NONE"
@@ -62,11 +67,34 @@ for row in rows:
             team = elements[1].text
             #dict2022[name] = team
             info.append(team)
-        for cell in row:            
+        for cell in rows[row]:            
             data_type = cell.get("data-stat")
             if data_type in stats_set:
                 info.append(cell.text)
         info += teams[team]
-
+        for cell in rows_per[row]:
+            data_type = cell.get("data-stat")
+            if data_type == "def_rtg":
+                info.append(cell.text)
         dict2022[name] = info
+# for row in rows:
+#     info = []
+#     if "thead" not in row.get("class"):
+#     # players.append(row.find_all("a")[0].text)
+#         elements = row.find_all("a")
+#         name = elements[0].text
+#         if len(elements) == 1:
+#             #dict2022[name] = "NONE"
+#             info.append("NONE")
+#         else:
+#             team = elements[1].text
+#             #dict2022[name] = team
+#             info.append(team)
+#         for cell in row:            
+#             data_type = cell.get("data-stat")
+#             if data_type in stats_set:
+#                 info.append(cell.text)
+#         info += teams[team]
+
+#         dict2022[name] = info
 print(dict2022)
